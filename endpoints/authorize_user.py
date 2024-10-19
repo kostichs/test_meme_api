c1b3ceec69559ef498ca2048dc44a12d53d39e60
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-19 15:43:34 +0500

    Modified Authorization class

--- endpoints/authorize_user.py
+++ endpoints/authorize_user.py
@@ -4,15 +4,16 @@ import requests
 from endpoints.endpoint import Endpoint
 
 
-class Authorization(Endpoint):
+class AuthorizeUser(Endpoint):
     CREDENTIALS_FILE = 'credentials.json'
 
     def get_token(self):
         self.token = self.__get_token_from_file()
         if self.token:
-            return self.token
-
-        self.token = self.__fetch_token_from_page()
+            if self.is_token_alive():
+                return self.token
+            else:
+                self.token = self.__fetch_token_from_page()
         self.__save_token_to_file(self.token)
         return self.token
 
@@ -41,6 +42,5 @@ class Authorization(Endpoint):
         return True if self.response.status_code == 200 else False
 
 
-test = Authorization()
+test = AuthorizeUser()
 print(test.get_token())
-print(test.is_token_alive())
