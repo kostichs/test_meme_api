e78e679f32a769d832a733e9d1ecc703092b060d
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-19 15:07:49 +0500

    Temporarily added authorization checks in Authorization class

---
+++ endpoints/authorization.py
@@ -0,0 +1,46 @@
+import os
+import json
+import requests
+from endpoints.endpoint import Endpoint
+
+
+class Authorization(Endpoint):
+    CREDENTIALS_FILE = 'credentials.json'
+
+    def get_token(self):
+        self.token = self.__get_token_from_file()
+        if self.token:
+            return self.token
+
+        self.token = self.__fetch_token_from_page()
+        self.__save_token_to_file(self.token)
+        return self.token
+
+    def __get_token_from_file(self):
+        if os.path.exists(self.CREDENTIALS_FILE):
+            with open(self.CREDENTIALS_FILE, 'r', encoding='utf-8') as file:
+                credentials = json.load(file)
+                return credentials.get("token")
+        return None
+
+    def __fetch_token_from_page(self):
+        name = {"name": "sergey"}
+        self.response = requests.post(self.url + 'authorize', json=name)
+
+        if self.response.status_code == 200:
+            return self.response.json()["token"]
+        else:
+            raise Exception("Failed to retrieve token: " + self.response.text)
+
+    def __save_token_to_file(self, token):
+        with open(self.CREDENTIALS_FILE, 'w', encoding='utf-8') as file:
+            file.write(json.dumps({"token": token}, ensure_ascii=False))
+
+    def is_token_alive(self):
+        self.response = requests.get(f"{self.url}authorize/{self.token}")
+        return True if self.response.status_code == 200 else False
+
+
+test = Authorization()
+print(test.get_token())
+print(test.is_token_alive())
---
+++ endpoints/endpoint.py
@@ -0,0 +1,7 @@
+class Endpoint:
+    url = 'http://167.172.172.115:52355/'
+    response = None
+    response_json = None
+    token = None
+    headers = None
+    default_data = None
