106b09afd65f159a897f48bda904059d9899a204
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-19 22:07:45 +0500

    Added some steps for full_change_meme.py

--- root/endpoints/full_change_meme.py
+++ root/endpoints/full_change_meme.py
@@ -1,14 +1,56 @@
+import allure
 import requests
 from root.endpoints.endpoint import Endpoint
 
 
 class FullChangeMeme(Endpoint):
 
+    @allure.step('Change meme with PUT method')
     def change_meme(self, data, meme_id):
         data['id'] = meme_id
         self.response = requests.put(
-            f"{self.url}/meme/{meme_id}",
+            f"{self.url}meme/{meme_id}",
             json=data,
             headers={"Authorization": self.token}
         )
-        assert self.response.status_code == 200, f"Unexpected status code: {self.response.status_code}"
+        try:
+            self.response_json = self.response.json()
+        except requests.exceptions.JSONDecodeError:
+            self.response_json = ""
+
+    @allure.step('Check parameter values in the updated meme')
+    def check_response_values(self, data):
+        for key, value in data.items():
+            assert str(self.response.json()[key]) == str(
+                value), f"Parameter {key} with {self.response.json()[key]} value is wrong: {value}"
+
+    @allure.step('Check response structure')
+    def check_response_structure(self, expected_keys):
+        response_json = self.response.json()
+        for key in expected_keys:
+            assert key in response_json, f"Parameter '{key}' not found in response"
+
+    @allure.step('Check specific parameter value')
+    def check_specific_parameter(self, key, expected_value):
+        assert self.response.json()[key] == expected_value, \
+            f"Parameter '{key}' is not '{self.response.json()[key]}'"
+
+    @allure.step('Check for errors with invalid data')
+    def check_invalid_data(self, data, meme_id):
+        self.response = requests.put(
+            f"{self.url}meme/{meme_id}",
+            json=data,
+            headers={"Authorization": self.token}
+        )
+        assert self.response.status_code == 404, f"Unexpected status code: {self.response.status_code}"
+
+    @allure.step('Check for required parameters')
+    def check_missing_required_parameters(self, missing_params, meme_id):
+        for param in missing_params:
+            data = {k: v for k, v in self.response_json.items() if k != param}
+            response = requests.put(
+                f"{self.url}meme/{meme_id}",
+                json=data,
+                headers={"Authorization": self.token}
+            )
+            assert response.status_code == 404, f"Unexpected status code: {response.status_code}"
