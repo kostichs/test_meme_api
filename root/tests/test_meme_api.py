df25f2d90f1056a5a44fc78a2747659c07386093
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-19 19:37:12 +0500

    Added meme datas in json

--- root/tests/test_meme_api.py
+++ root/tests/test_meme_api.py
@@ -1,5 +1,14 @@
 import pytest
 import requests
+import json
+import os
+
+
+def load_meme_data(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+MEME_DATA = load_meme_data(os.path.join(os.path.dirname(__file__), '../data/meme_structure.json'))
 
 
 def test_get_all_memes(get_memes_endpoint):
@@ -11,24 +20,18 @@ def test_get_one_meme(get_meme_by_id_endpoint, meme_id):
     get_meme_by_id_endpoint.get_one_meme(meme_id=meme_id)
 
 
-def test_create_meme(create_meme_endpoint):
-    info = {
-        "colors": [
-            "green",
-            "black",
-            "white"
-        ],
-        "objects": [
-            "picture",
-            "text"
-        ]
+@pytest.mark.parametrize("meme", MEME_DATA)
+def test_create_meme(create_meme_endpoint, meme):
+    data = {
+        "url": meme["url"],
+        "text": meme["text"],
+        "tags": meme["tags"],
+        "info": meme["info"]
     }
-    tags = [
-        "fun",
-        "yoda"
-    ]
-    url = 'https://i.pinimg.com/originals/a6/56/23/a65623b21bd407284697726d01f39c7e.jpg'
-    text = "Sample text"
-    data = {"url": url, "text": text, "tags": tags, "info": info}
     create_meme_endpoint.create_a_meme(data)
-    response = requests.delete(f"{create_meme_endpoint.url}meme/{create_meme_endpoint.response.json()['id']}", headers={"Authorization": create_meme_endpoint.token})
+
+    response = requests.delete(
+        f"{create_meme_endpoint.url}meme/{create_meme_endpoint.response.json()['id']}",
+        headers={"Authorization": create_meme_endpoint.token}
+    )
+    assert response.status_code == 200
